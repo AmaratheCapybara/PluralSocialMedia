b40d067b1f4b3de8e6b2fdadc1a912cb48e6a82b
Hopetheheartmagicmusician <speedracerpablo@gmail.com>
2024-10-19 18:59:36 -0400

    created SetProfileType function

--- HeadmateCreation.js
+++ HeadmateCreation.js
@@ -6,3 +6,15 @@ const AgeRange = document.querySelector ("AgeRange")
 const Fictive = document.querySelector ('Fictive')
 const Introject = document.querySelector ("Introject")
 const Bio = document.querySelector ("Bio")
+
+// create new Profile
+ProfileType = ['Planet', 'Moon', 'Star', 'Satellite']
+// Set Profile Type
+function SetProfileType (){
+if (AgeRange = "Little" || Age < 13)
+    ProfileType = 'Satellite'
+if (AgeRange = 'Teen' || Age < 18 && Age > 12) {
+    ProfileType = 'Moon'
+} else {ProfileType = 'Planet' 
+}   
+}
